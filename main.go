47e5041574bc081cc5ddc67f501fc6039e59078f
adrisalas <45205815+adrisalas@users.noreply.github.com>
2022-02-19 16:53:10 +0100

    Add for loops

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"strings"
 )
 
 func main() {
@@ -11,28 +11,34 @@ func main() {
 	var tickets = TOTAL_TICKETS
 
 	fmt.Print("> App initialized!\n")
-	fmt.Printf("> This is a tutorial for %v's Go booking app\n", name)
-	fmt.Println("\nWe have", TOTAL_TICKETS, "tickets and", tickets, "tickets are available to purchase")
-	fmt.Fprint(os.Stdout, "####################################\n\n")
+	fmt.Printf("> This is a tutorial for %v's Go booking app\n\n", name)
+	fmt.Printf("We have %v tickets and %v tickets are tickets are available to purchase\n", TOTAL_TICKETS, tickets)
+	fmt.Printf("####################################\n\n")
 
-	//var bookings [50]string ARRAY
-	//var bookings []string SLICE (dynamic array)
 	var bookings = []string{}
+	var userNames []string
+	for {
+		var userName string
+		var email string
+		var ticketsToBuy int
 
-	var userName string
-	var email string
-	var ticketsToBuy int
+		fmt.Print("Your username: ")
+		fmt.Scan(&userName) // & <-- Get memory address, pointer
+		fmt.Print("Your email: ")
+		fmt.Scan(&email)
+		fmt.Print("Number of tickets: ")
+		fmt.Scan(&ticketsToBuy)
 
-	fmt.Print("Your username: ")
-	fmt.Scan(&userName) // & <-- Get memory address, pointer
-	fmt.Print("Your email: ")
-	fmt.Scan(&email)
-	fmt.Print("Number of tickets: ")
-	fmt.Scan(&ticketsToBuy)
-	tickets = tickets - uint(ticketsToBuy)
-	bookings = append(bookings, userName+": "+email)
-	fmt.Fprintf(os.Stdout, "%v bought %v tickets\n", userName, ticketsToBuy)
-	fmt.Printf("The slice: %v\n", bookings)
-	fmt.Printf("The slice size: %v\n", len(bookings))
+		tickets = tickets - uint(ticketsToBuy)
+		bookings = append(bookings, userName+" "+email)
+		fmt.Printf("\n%v [%v] bought %v tickets\n", userName, email, ticketsToBuy)
+		fmt.Printf("There are %v tickets left\n", tickets)
 
+		for _, booking := range bookings {
+			var user = strings.Fields(booking)
+			username := user[0]
+			userNames = append(userNames, username)
+		}
+		fmt.Printf("Users that booked a ticket are: %v\n\n", userNames)
+	}
 }
