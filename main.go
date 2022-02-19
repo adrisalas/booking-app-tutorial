a37ca4ddb776e4ae480fed0ada91cfe4999407dd
adrisalas <45205815+adrisalas@users.noreply.github.com>
2022-02-19 15:28:13 +0100

    Add array/slice

--- main.go
+++ main.go
@@ -15,14 +15,24 @@ func main() {
 	fmt.Println("\nWe have", TOTAL_TICKETS, "tickets and", tickets, "tickets are available to purchase")
 	fmt.Fprint(os.Stdout, "####################################\n\n")
 
+	//var bookings [50]string ARRAY
+	//var bookings []string SLICE (dynamic array)
+	var bookings = []string{}
+
 	var userName string
+	var email string
 	var ticketsToBuy int
 
 	fmt.Print("Your username: ")
 	fmt.Scan(&userName) // & <-- Get memory address, pointer
+	fmt.Print("Your email: ")
+	fmt.Scan(&email)
 	fmt.Print("Number of tickets: ")
 	fmt.Scan(&ticketsToBuy)
-	fmt.Println("Your username was saved in", &userName, "memory address")
-	fmt.Fprintf(os.Stdout, "%v wants to buy %v tickets\n", userName, ticketsToBuy)
+	tickets = tickets - uint(ticketsToBuy)
+	bookings = append(bookings, userName+": "+email)
+	fmt.Fprintf(os.Stdout, "%v bought %v tickets\n", userName, ticketsToBuy)
+	fmt.Printf("The slice: %v\n", bookings)
+	fmt.Printf("The slice size: %v\n", len(bookings))
 
 }
